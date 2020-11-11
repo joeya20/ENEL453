9a4c7581a3e2c341de59012f7b51da58ebae83df
joeya20 <56373482+joeya20@users.noreply.github.com>
2020-11-11 11:45:54 -0700

    Completed RTL design code

--- ENEL 453 Lab 3/blank_select.vhd
+++ ENEL 453 Lab 3/blank_select.vhd
@@ -2,18 +2,18 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity blank_select is
-    Port (
-				num1       	: in  STD_LOGIC_VECTOR(3 downto 0);
-				num2     	: in  STD_LOGIC_VECTOR(3 downto 0);
-				blank_out	: out STD_LOGIC_VECTOR(5 downto 0)
-          );
+	Port(
+		state			: in  STD_LOGIC_VECTOR(1 downto 0);
+		num1       	: in  STD_LOGIC_VECTOR(3 downto 0);
+		num2     	: in  STD_LOGIC_VECTOR(3 downto 0);
+		blank_out	: out STD_LOGIC_VECTOR(5 downto 0)
+	);
 end blank_select;
 
 
 architecture behavioural of blank_select is
-begin 
-
-		blank_out <= 	"111100" when (num1 = "0000" and num2 = "0000") else
-							"111000" when (num1 = "0000" and num2 /= "0000") else
+begin
+		blank_out <= 	"111100" when (num1 = "0000" and num2 = "0000" and state = "11") else
+							"111000" when ((num1 = "0000" and state = "01") or (num1 = "0000" and num2 /= "0000" and state = "11")) else
 							"110000";
 end behavioural;
